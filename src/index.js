6f1cf8e2148606a2cad9ca03dd4e559cbbcf1732
John Hornijas <gon3000@gmail.com>
2020-09-22 14:18:12 -0400

    Deployed to Firebase

--- src/index.js
+++ src/index.js
@@ -4,6 +4,8 @@ import './index.css';
 // import Question from './question.js';
 import * as serviceWorker from './serviceWorker';
 //import * as firebase from "firebase/app";
+import {ReactComponent as Restart} from "./refresh-24px.svg";
+import {ReactComponent as NoWifi} from "./wifi_off-black-24dp.svg"
 import "firebase/auth";
 import "firebase/messaging";
 
@@ -12,7 +14,7 @@ function getCategories(nextPage) {  //TODO Add pagination for categories
                                     //TODO Add error catching
     //let time = new Date();
     let offset = "offset=" + nextPage;
-    return fetch("http://jservice.io/api/categories?count=6;"+ offset)
+    return fetch("https://jservice.io/api/categories?count=6;"+ offset)
         .then(function(response){ 
             if (response.ok)
             return response.json();
@@ -25,28 +27,45 @@ function getCategories(nextPage) {  //TODO Add pagination for categories
         })
         .catch(function(error){
             console.error(error, "No connection");
-            displayError();
+            return "error";
         })
         
 }
 function getClues(id){ //TODO Add error catching
     var category = "id=";
-    var data = Promise.all( id.map( async element=> {return await (fetch("http://jservice.io/api/category?"+ category+element.id+';')
+    var data = Promise.all( id.map( async element=> {return await (fetch("https://jservice.io/api/category?"+ category+element.id+';')
                                 .then(response => response.json())
                                 .then(function (data) {
                                     console.log('getClues', data)
+                                    data.clues.forEach(element => { 
+                                        if(!element.value){throw new Error("Value is null")}
+                                    });
                                     return data;}))}))
-                                .catch(function(error, element){ 
+                                .catch(function(error){ 
                                     console.error(error);
+                                    return null;
                                 })
     return data;        
 }
-function displayError() {
-    return ReactDOM.render(
+class DisplayError extends React.Component{  //Error Boundary
+    constructor(props){
+        super(props);
+        this.state ={ noInternet: props.noInternet};
+        }
+
+    render(){
+        if(this.state.noInternet){
+        return (
+        <div>
+        {/* <svg id="NoWifi"/> */}
+        <NoWifi className="nowifi"></NoWifi>
         <div id="error">
-            <p>Apologies, Jeoparody requires internet connection.</p>
-        </div>, document.getElementById("root"));
+            <p>Jeoparody requires internet connection.<br/>Please check your connection</p>
+            <button onClick={this.props.onClick}>Retry</button>
+        </div></div>)}
     
+         return this.props.children;
+    }
 }
 
 
@@ -66,8 +85,8 @@ class Column extends React.Component {
 }
 function Podium(props){
     return(<div className="podium">
-                <p id="score">{props.score}</p>
-                <p id="player">John</p>
+                <p id="score" title="Score">${props.score}</p>
+                <p id="player">{props.name}</p>
             </div>)
 }
 //TODO make an Answer input component 
@@ -87,6 +106,15 @@ function Question(props){
             {props.correctAnswerIsActive ? null : <Answer onChange={props.onChange} timer={props.timer} answer={props.question}/>}
             </>
         }
+
+function Player(props){
+    return (<div className={props.className}>
+                <form id="form" onSubmit={props.handleSubmit}>
+                    <input id="name" type="text" form="form" autoComplete="off" autoFocus={true} placeholder="Name" />
+                    <button id="submit" type="button" onClick={props.onClick} value="Start" >Start</button>
+                </form>
+            </div>)
+}
  
 class Board extends React.Component {
     constructor(props) {
@@ -97,21 +125,45 @@ class Board extends React.Component {
             value: null,
             questionsLeft: 30,
             round: 1,
-            restart: ".restart",
             timerValue: 0,
+            nameActive: false,
+            name: "You",
+            loading: false,
+            finalScore: false
         };
-        this.showQuestion = this.showQuestion.bind(this);
+        //this.showQuestion = this.showQuestion.bind(this);
         this.checkAnswer = this.checkAnswer.bind(this);
         this.nextRound = this.nextRound.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
         
+    }
+
+    handleSubmit(e){
+        e.preventDefault();
+        this.start();
+    }
+
+    endGame(){
+        //document.querySelector("dialog").showModal();
+        this.setState({playing: false, nameActive: false, finalScore: true, round: 1})        
+    }
+    toggleDialog(open){
+        if(open){this.setState({finalScore: true})};
+        this.setState({finalScore: false, score: 0});
+
     }
     
     async nextRound(){ //TODO restart the board after for next round
+        if(this.state.round > 3) {
+            this.endGame();
+            return;}
         this.setState({offset: this.state.offset + 6});
         let nextPage = this.state.offset;
         let categories = await getCategories(nextPage);
+        if (!categories) return;
         let clues = await getClues(categories);
-        this.setState({clue: clues, category: categories, questionsLeft: 29});
+        if(!clues){return this.nextRound()}
+        this.setState({clue: clues, category: categories, questionsLeft: 30});
         this.createColumn();
         this.renderValues();
         //enable all the value buttons
@@ -125,28 +177,31 @@ class Board extends React.Component {
                     //TODO make the right answer in parenthesis as optional
         const answer = document.getElementById("answer").value.toLowerCase();
         // eslint-disable-next-line
-        const correct = answer.replace(/a\s|the\s|an\s|<i>|<\/i>/, "") == this.state.answer.toLowerCase().replace(/a\s|the\s|an\s|<i>|<\/i>/, ""); 
-        console.log("answer",answer.replace(/a\s|the\s|an\s|<i>|<\/i>/, ""))
-        console.log("state.answer", this.state.answer.toLowerCase().replace(/a\s|the\s|an\s|<i>|<\/i>/, ""));
-        console.log(this.state.answer.replace(/a\s|the\s|an\s|<i>|<\/i>/, ""))
-        console.info("Answer is:", this.state.answer);
-        console.info(answer);
+        const correct = answer.toLowerCase().replaceAll(/a\s|the\s|an\s|<i>|<\/i>/g, "") == this.state.answer.toLowerCase().replaceAll(/a\s|the\s|an\s|<i>|<\/i>/g, ""); 
+        console.info("Raw Answer: ",answer)
+        console.log("User answer:", this.state.answer.toLowerCase());
+        console.info("Correct answer:", answer.toLowerCase().replaceAll(/a\s|the\s|an\s|<i>|<\/i>/g, ""));
         const value = this.state.value;
         console.info(typeof value);
         console.log(correct ? " Answer is correct!" : "Answer is wrong!");
-        var score = correct ? this.state.score + value : this.state.score - value; 
-        this.setState({score: score})
+        let score;
+        if (correct) 
+            {score = this.state.score + value;
+            this.setState({score: score})}
         console.info("score",this.state.score);
         this.setState({correctAnswerIsActive: true});
         setTimeout(()=>{this.setState({correctAnswerIsActive: false})},5000);
-
-        if(this.state.questionsLeft === 0) this.nextRound();
+        if(this.state.questionsLeft === 0){
+            this.setState({round: this.state.round + 1})
+            this.nextRound();
+        } 
     }
 
-    showQuestion(id, ind, value) {
+    showQuestion(e, id, ind, value) {
          
          var question = this.state.clue.find(function (clues) {                
-              return (clues.clues[ind].category_id === id);
+              return (clues.id === id)
+               // clues[ind].category_id === id);
          });
          console.log("Value:", value + " Is"+ typeof value)
          this.setState({questionIsActive: true, question: question.clues[ind].question, answer: question.clues[ind].answer, value: value});
@@ -159,19 +214,18 @@ class Board extends React.Component {
                 }
             }, 2000);
          //TODO sync setTimeout with timer progress bar
-         let element = document.getElementById(id).children[ind];
-         element.disabled =true;
-         element.innerText="";
-         this.setState({questionsLeft: this.state.questionsLeft - 1});
-         if(this.state.questionsLeft === 0){this.nextRound()}
-         
+         e.currentTarget.disabled=true;
+         e.currentTarget.innerText="";
+        //  let element = document.getElementById(id).children[buttonId];
+        //  element.disabled=true;
+        //  element.innerText="";
+         this.setState({questionsLeft: this.state.questionsLeft - 1});         
     }
 
     renderValues(){
         //let clue;
         return ReactDOM.render(
             /*TODO add key to children
-             * empty value buttons after question is answered
              * Refactor as a function component
              * Use Ref to disable and clear button
              * check if value is null, fix if null*/
@@ -182,83 +236,108 @@ class Board extends React.Component {
                             <div id={category.id}>{
                                 
                             <React.Fragment key={category.id}>
+                           
+                                {this.qualifyClues(category.clues)} 
                                 
-                                {/* {clue = category.clues.filter(element => element.value != null)}
-                                {console.info('column', clue)}
-                                {this.fixNUll()} */}
-                                
-                                <button  id={0} key={category.clues[0].value} onClick={()=>this.showQuestion(category.id,0,category.clues[0].value)}>{'$'+category.clues[0].value}</button>
-                                <button id={1} key={category.clues[1].value} onClick={()=>this.showQuestion(category.id,1,category.clues[1].value)}>{'$'+category.clues[1].value}</button>
-                                <button id={2} key={category.clues[2].value} onClick={()=>this.showQuestion(category.id,2,category.clues[2].value)}>{'$'+category.clues[2].value}</button>
-                                <button id={3} key={category.clues[3].value} onClick={()=>this.showQuestion(category.id,3,category.clues[3].value)}>{'$'+category.clues[3].value}</button>
-                                <button id={4} key={category.clues[4].value} onClick={()=>this.showQuestion(category.id,4,category.clues[4].value)}>{'$'+category.clues[4].value}</button>
                             </React.Fragment>}
                             </div>), document.querySelector('#amount')
            );
         
     }
     
-    // Return an actual value instead of null. Maybe convert as s bonus question with X3 value.
-    fixNUll(clues, index){
-        if(index === 0){return (clues[index+2].value - clues[index+1].value)}
-        else if(index === 4){return (clues[index-1].value - clues[index-2].value)}
-        else {return (clues[index+1].value - clues[index-1].value)*(index-1)}
+    // Return an actual value instead of null. Maybe convert as s bonus question with *3 value.
+    qualifyClues(clues){
+        let column = [] ;
+        let prev, a = 0;
+
+        clues.sort(function(a,b){return a.value-b.value})
+        try {
+            for (let i = 0; column.length < 5; i++) {       // prevents duplicate values
+                if (clues[i].value === prev) { 
+                    prev = clues[i].value;
+                    continue;
+                } 
+                column[a] = <button id={a} key={clues[i].value} onClick={(e) => this.showQuestion(e, clues[i].category_id, i, clues[i].value)}>{'$' + clues[i].value}</button>
+                    prev = clues[i].value;
+                    a++;       
+            }
+                
+            } catch (error) {
+                console.error(error);
+                return this.start();
+            }
+        return column;
     }
 
     createColumn() {
         return ReactDOM.render(<Column item={this.state.category}/>, document.getElementById('categories'));
         
     }
+
+    play(){
+        this.setState({playing: false, noInternet: false, loading: false});
+        this.setState({nameActive: true})       
+    }
     
     
     async start() { //TODO add a Name Form
+        //if(!document.getElementById("loading").open) document.getElementById("loading").showModal();
+        this.setState({loading: true})
+        const name = document.getElementById("name").value;
+        this.setState({name: name});
         let startOffset = Math.floor(Math.random() *(18409) + 1);
-        this.setState({offset: startOffset});
+        this.setState({offset: startOffset, score: 0});
         console.info("Starting ID: ", startOffset)
         var categories = await getCategories(startOffset);
-        if (!categories) return;
-        console.log('categories',categories.map(obj => obj.id)); 
+        if (!categories)return;
+        if(categories ==="error"){this.setState({noInternet: true}); return};
+        //else this.setState({noInternet: false});
+        console.log('categories',categories.map(obj => obj.id));
         var clues = await getClues(categories)
+        if(!clues){ return this.start()}
         console.log('clues',clues);
-        this.setState({ category: categories, playing: true, clue: clues});
+        this.setState({ category: categories, playing: true, clue: clues, nameActive: false});
         this.createColumn();
         this.renderValues();
-
+        this.setState({loading: false})
+        //document.getElementById("loading").close();
     }
 
     render() {
         const intro = "JEOPARODY" //TODO Redesign the logo
+        if(!this.state.noInternet){
         return (
             <div>
-                {this.state.playing ? null :  <div><h1 className="title">{intro}</h1><button className="play" value="PLAY" onClick={() => this.start()}>Play</button></div>}
-
-                {/* {this.state.playing ? <output id="score">{this.state.score}</output> : null} */}
+                
+                {this.state.playing ? null :  <div><h1 className="title">{intro}</h1><button className="play" value="PLAY" onClick={()=>this.play()}>Play</button></div>}
+                {this.state.nameActive ? <Player className="askName-active" onClick={() => this.start()} handleSubmit={this.handleSubmit}/> : null}
+                {/* {this.state.loading ? document.querySelector("dialog").showModal() : null } */}
+                {this.state.finalScore ? <dialog id="final-score" open>Your Score<br></br> <p>${this.state.score}</p><button onClick={()=>this.toggleDialog(false)}>close</button></dialog> : null}
+                {this.state.loading ? <dialog id="loading" autoFocus="true" open>Loading</dialog> : null}
+               
                 {this.state.playing ? 
                     <div className="stage" >
+                        <div id="top-header"></div>
                         <div id="left-post"></div>
                         <div id="right-post"></div>
-                        <button id="restart" onClick={()=>this.start()}>Restart</button>
+                        <DisplayError>
+                        <button id="restart" title="Restart" onClick={()=>this.play()}><Restart className="restart-svg"></Restart></button>
+                        </DisplayError>
+                        <div id="round" title="Round"><b>{this.state.round}</b></div>
                         <div className ="game">
                             <div id='categories'></div>
                             <div id="amount"></div>
-                            <Podium score={this.state.score}></Podium>
+                            <Podium score={this.state.score} name={this.state.name}></Podium>
                             {this.state.questionIsActive ? <Question className={"question-active"} /* onChange={()=>this.checkAnswer} */ timer={this.state.timerValue} question={this.state.question}/> : null}
-                            {this.state.correctAnswerIsActive ? <Question className={"question-active"} correctAnswerIsActive={this.state.correctAnswerIsActive} question={this.state.answer}/> : null}
+                            {this.state.correctAnswerIsActive ? <Question className={"question-active"} correctAnswerIsActive={this.state.correctAnswerIsActive} question={this.state.answer.replaceAll(/<i>|<\/i>|\\/g,"")}/> : null}
                         </div>
 
                     </div> : null}
             </div>
-        );
+        );}
+        return <DisplayError noInternet={this.state.noInternet} onClick={()=>this.play()}></DisplayError>
     }
 }
-// eslint-disable-next-line
-{/* <div className="game" >
-                    <div id='categories'></div>
-                    <div id="amount"></div>
-                    {this.state.questionIsActive ? <Question className={"question-active"} /* onChange={()=>this.checkAnswer}  question={this.state.question}/> : null}
-                    //{this.state.correctAnswerIsActive ? <Question className={"question-active"} /* onChange={()=>this.checkAnswer}  question={this.state.answer}/> : null}
-
-               // </div> */}
 
 class Screen extends React.Component {
     render() {
